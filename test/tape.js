ffba6afe6f76e098af6497122d7b4f24d969c89c
Nathan Winch <nwinch@gmail.com>
2015-09-24 10:55:09 +1000

    Add test envs to helper

--- test/tape.js
+++ test/tape.js
@@ -1,6 +1,9 @@
 var test = require('tape');
 var tapSpec = require('tap-spec');
 
+process.env.NODE_ENV = 'test';
+process.env.PORT = 3010;
+
 test.createStream()
   .pipe(tapSpec())
   .pipe(process.stdout);
