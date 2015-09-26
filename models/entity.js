f4150408e372f84ea2fc576fcf303c63e61d3364
lyntco <strawww@gmail.com>
2015-09-26 15:26:43 +1000

    Stub enity model

---
+++ models/entity.js
@@ -0,0 +1,22 @@
+// var db = require('root/lib/db');
+
+
+exports.create = () => {};
+
+exports.getById = () => {};
+
+exports.getByEmail = () => {};
+
+exports.getByPermissions = () => {};
+
+exports.addPermission = () => {};
+
+exports.addInheritedPermission = () => {};
+
+exports.update = () => {};
+
+exports.removePermission = () => {};
+
+exports.removeInheritedPermission = () => {};
+
+exports.delete = () => {};
