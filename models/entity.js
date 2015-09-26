5d6950408cdfaa22b9f7e8ab71d8d3ac22e20489
lyntco <strawww@gmail.com>
2015-09-26 20:06:43 +1000

    Implement db methods into entity model

--- models/entity.js
+++ models/entity.js
@@ -5,22 +5,47 @@ exports.create = (entity) => {
   return db.createEntity(entity);
 };
 
-exports.getById = () => {
-  // return db.createEntity(entity);
+exports.getById = (id) => {
+  return db.getEntity(id);
 };
 
-exports.getByEmail = () => {};
+exports.getByEmail = (email) => {
+  return db.getEntityByEmail(email);
+};
 
-exports.getByPermissions = () => {};
+exports.getByPermission = (permission) => {
+  if (permission.type && permission.entity)
+    return db.getEntitiesByPermission;
+  else if (permission.type)
+    return db.getEntitiesByPermissionType;
+  else if (permission.entity)
+    return db.getEntitiesByPermissionEntity;
+};
 
-exports.addPermission = () => {};
+exports.update = (entity) => {
+  return db.updateEntity(entity);
+};
 
-exports.addInheritedPermission = () => {};
+exports.getPermissions = (id) => {
+  return db.getPermissions(id);
+};
 
-exports.update = () => {};
+exports.addPermission = (id, permission) => {
+  return db.addPermission(id, permission);
+};
 
-exports.removePermission = () => {};
+exports.addInheritedPermission = (id, permission) => {
+  return db.addInheritedPermission(id, permission);
+};
 
-exports.removeInheritedPermission = () => {};
+exports.removePermission = (id, permission) => {
+  return db.removePermission(id, permission);
+};
 
-exports.delete = () => {};
+exports.removeInheritedPermission = (id, permission) => {
+  return db.removeInheritedPermission(id, permission);
+};
+
+exports.delete = (id) => {
+  return db.deleteEntity(id);
+};
