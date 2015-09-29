8424a780f096ec2cc8afcd82601ef3ebf4dbe674
lyntco <strawww@gmail.com>
2015-09-29 15:50:57 +1000

    Add error catching to controllers and remove permissions urls

--- controllers/entities.js
+++ controllers/entities.js
@@ -8,58 +8,60 @@ exports.route = function(server) {
   server.get('/entities/:id', exports.read);
   server.put('/entities/:id', exports.update);
   server.del('/entities/:id', exports.delete);
-
-  server.post('/entities/:id/permissions', schemaValidation, exports.createPermission);
-  server.get('/entities/:id/permissions', exports.readPermissions);
-  server.del('/entities/:id/permissions', exports.deletePermission);
-
 };
 
 exports.create = function(req, res, next) {
   return entity.create(req.body)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
 
 exports.read = function(req, res, next) {
     return entity.getById(req.params.id)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
 
 exports.update = function(req, res, next) {
     return entity.update(req.body)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
 
 exports.delete = function(req, res, next) {
     return entity.delete(req.params.id)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
 
 exports.createPermission = function(req, res, next) {
     return entity.createPermission(req.params.id, req.body)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
 
 exports.readPermissions = function(req, res, next) {
     return entity.getPermissions(req.params.id)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
 
 exports.deletePermission = function(req, res, next) {
     return entity.deletePermission(req.params.id)
     .then(doc => {
       res.send(doc);
-    });
+    })
+    .catch(next);;
 };
