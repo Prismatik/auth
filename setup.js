5c6a713b40ac3069d5fc07d254f0a691ae3d457e
Nick Matenaar <nick@prismatik.com.au>
2015-11-18 18:35:32 +1100

    Add indexs to setup

--- setup.js
+++ setup.js
@@ -18,4 +18,29 @@ module.exports = function setUp() {
     throw err;
   })
   .then(() => r.table('entities').indexWait('emails').run())
+  .then(() => r.table('entities').indexCreate('permissions', e => {
+    return e('permissions').map(p => [p('type'), p('entity')])
+  }, { multi: true }).run())
+  .catch(err => {
+    if (err.message.indexOf('Index `permissions` already exists') > -1 )return;
+    throw err;
+  })
+  .then(() => r.table('entities').indexWait('permissions').run())
+  .then(() => r.table('entities').indexCreate('inherited_permissions', e => {
+    return e('inherited_permissions').map(p => [p('type'), p('entity')])
+  }, { multi: true }).run())
+  .catch(err => {
+    if (err.message.indexOf('Index `inherited_permissions` already exists') > -1 )return;
+    throw err;
+  })
+  .then(() => r.table('entities').indexWait('inherited_permissions').run())
+  .then(() => r.table('entities').indexCreate('all_permissions', e => {
+    return e('permissions').map(p => [p('type'), p('entity')])
+      .add(e('inherited_permissions').map(p => [p('type'), p('entity')]));
+  }, { multi: true }).run())
+  .catch(err => {
+    if (err.message.indexOf('Index `all_permissions` already exists') > -1 )return;
+    throw err;
+  })
+  .then(() => r.table('entities').indexWait('all_permissions').run())
 }
